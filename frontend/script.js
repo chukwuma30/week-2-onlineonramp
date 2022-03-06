1dc514696f4c78a07dd97a2e53ec5f25d1a17141
Chukwuma Innocent <chukwuma30@gmail.com git config --global user.name Innocent>
2022-03-06 16:15:19 +0100

    new commit

---
+++ frontend/script.js
@@ -0,0 +1,24 @@
+const cancel = document.querySelector('.cancel');
+
+const send = document.querySelector('.send');
+
+let name = document.querySelector('input[type=text]');
+
+let email = document.querySelector('input[type=email]');
+
+let number = document.querySelector('input[type=tel]');
+/*
+let msg = document.getElementById('msg');
+cancel.addEventListener('click'.() => {
+    name.value = '';
+    email.value = '';
+    number.value = '';
+    msg.value = '';
+});
+
+send.addEventListener('click'.() => {
+            let msg Arr = Array.from(msg.value);
+
+            if (email.value = '' || name.value = '
+                ' || msg = .value)
+            });*/
